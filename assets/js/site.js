c432476d1f37949803f4a26d7ce6538503b1a961
Baxter <baxterj405@gmail.com>
2013-01-26 19:55:34 +0000

    index page including jquery slider

--- assets/js/site.js
+++ assets/js/site.js
@@ -19,24 +19,34 @@ $('#loginPage, #registerPage, #forgotPage').live('pageshow', function(event, dat
 	redirectLoggedIn()
 })
 
+$('#indexPage').live('pageshow', function(event, data){
+	$('#slider').nivoSlider({
+		effect: 'fade',
+		pauseTime: 5000,
+		randomStart: true,
+		animSpeed: 500
+	})
+})
+
 function genFooterHtml(){
 	var html=''
 	html+='<div class="footer_links_holder">\n'
 	html+='<div class="footer_links">Links<br/>\n'
-	html+='<a href="#">Link 1</a><br/>\n'
-	html+='<a href="#">Link 2</a><br/>\n'
+	html+='<a href="http://www.ramblers.org.uk/">Ramblers Association</a><br/>\n'
+	html+='<a href="http://www.naturalengland.org.uk/ourwork/enjoying/countrysidecode/default.aspx">Countryside Code (England &amp; Wales)</a><br/>\n'
 	html+='</div>\n'
 	html+='<div class="footer_links">Help<br/>\n'
-	html+='<a href="#">Link 1rgereah</a><br/>\n'
-	html+='<a href="#">Link 2</a><br/>\n'
+	html+='<a href="FAQ.html">FAQ</a><br/>\n'
+	html+='<a href="forgot.html">Forgot Password</a><br/>\n'
+	html+='<a href="mailto:support@rambleonline.com">Support Email</a><br/>\n'
 	html+='</div>\n'
-	html+='<div class="footer_links">Contact Us<br/>\n'
-	html+='<a href="#">Link 1</a><br/>\n'
-	html+='<a href="#">Link 2afhafdhdh</a><br/>\n'
+	html+='<div class="footer_links">Developer Contact<br/>\n'
+	html+='<a href="mailto:baxterj405@gmail.com">Email</a><br/>\n'
+	html+='<a href="http://www.linkedin.com/pub/james-baxter/27/99/80b">LinkedIn</a><br/>\n'
 	html+='</div>\n'
 	html+='<div class="clear"></div>\n'
 	html+='</div>\n' //footer links holder
-	html += '<div class="footer_img"><img src="assets/images/rambleon_logo_sm_cropped.png" /></div>\n'
+	html += '<div class="footer_img"><img src="assets/images/rambleon-logo-footer.png" /></div>\n'
 	footerContent = html
 	return html
 }
